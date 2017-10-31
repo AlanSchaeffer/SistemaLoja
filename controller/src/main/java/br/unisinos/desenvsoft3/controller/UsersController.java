8b5543ac6033a782613dcf6b4af446cd8b2f8d9a
Nicolas Michels <michels.nicolas@gmail.com>
2017-10-31 14:07:30 -0200

    integrate dist with spring app

---
+++ controller/src/main/java/br/unisinos/desenvsoft3/controller/UsersController.java
@@ -0,0 +1,12 @@
+package br.unisinos.desenvsoft3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UsersController {
+	@RequestMapping("/users")
+	public String index() {
+		return "../index.html";
+	}
+}
